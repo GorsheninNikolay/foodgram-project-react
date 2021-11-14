2c2f754648e38c18eb08dd61e6b0d7aa94d38094
Николай Горшенин <kolya.gorshenin.2002@gmail.com>
2021-11-14 12:11:13 +0300

    Final

--- backend/foodgram_backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/foodgram_backend/settings.py
@@ -71,28 +71,16 @@ WSGI_APPLICATION = 'foodgram_backend.wsgi.application'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': os.environ.get('DB_ENGINE'),
-#         'NAME': os.environ.get('DB_NAME'),
-#         'USER': os.environ.get('POSTGRES_USER'),
-#         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-#         'HOST': os.environ.get('DB_HOST'),
-#         'PORT': os.environ.get('DB_PORT'),
-#     }
-# }
-
-DATABASES = { 
-
-    'default': { 
-
-        'ENGINE': 'django.db.backends.sqlite3', 
-
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'), 
-
-    } 
-
-} 
+DATABASES = {
+    'default': {
+        'ENGINE': os.environ.get('DB_ENGINE'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
+    }
+}
 
 
 if 'test' in sys.argv:
